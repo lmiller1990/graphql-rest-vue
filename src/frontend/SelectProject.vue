774ef382feaa7fbaa4b9ee91447e96dea3639711
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-07-24 20:26:09 +1000

    remove old method'

--- src/frontend/SelectProject.vue
+++ src/frontend/SelectProject.vue
@@ -9,7 +9,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { SelectProject } from './types'
-import { useStore } from './store'
 
 export default defineComponent({
   props: {
